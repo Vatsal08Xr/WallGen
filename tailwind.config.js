afed00a967760cb845d4bbab994a8677a509bf5f
Saivatsal <kohaku@Mac.lan>
2026-07-25 18:40:38 +0530

    perf: replace Tailwind CDN with prebuilt static CSS to eliminate page load delay

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./*.html", "./js/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Space Grotesk"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
